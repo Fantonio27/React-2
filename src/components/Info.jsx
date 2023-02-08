d0a10a4b24c3527ea6615469c65e85cfff065dee
Francis Antonio <113699783+Fantonio27@users.noreply.github.com>
2023-02-08 22:05:50 +0800

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,9 +5,9 @@ export default function Info(){
     return(
         <>
             <img src={pic} className="info--pic" />
-            <h1>Laura Smith</h1>
+            <h1>Francis Louie Antonio</h1>
             <h3>Frontend Developer</h3>
-            <h4>laurasmith.website</h4>
+            <h4>FrancisAntonio.website</h4>
             <div className="buttons">
                 <a href="https://mail.google.com/mail/u/0/" className="info--email">
                     <img src="https://img.icons8.com/material-rounded/16.5/1A1A1A/new-post.png" className="info--email--img"/>
